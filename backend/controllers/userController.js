f94e69ffaab72cc5f6ffcf5b3a3d3ce9c8975a79
KANIKA DROUNA <duskybook02@gmail.com>
2026-07-16 12:35:38 +0530

    user controller and router

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,3 +3,107 @@ import validator from "validator";
 import jwt from "jsonwebtoken";
 
 import userModel from "../models/userModel.js";
+
+
+
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
+const registerUser = async (req, res) => {
+    console.log("REGISTER API HIT");
+
+    try {
+
+        const { name, email, password } = req.body;
+        if (!validator.isEmail(email)) {
+            return res.json({
+                success: false,
+                message: "Please enter a valid email"
+            });
+        }
+        if (!validator.isLength(password, { min: 6 })) {
+            return res.json({
+                success: false,
+                message: "Password must be at least 6 characters long"
+            });
+        }
+
+        const exists = await userModel.findOne({ email });
+        if (exists) {
+            return res.json({
+                success: false,
+                message: "User already exists"
+            });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new userModel({
+            name,
+            email,
+            password: hashedPassword,
+            cartData: {}
+        });
+
+
+        const user = await newUser.save();
+        const token = createToken(user._id);
+
+        return res.json({
+            success: true,
+            token
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.json({
+            success: false,
+            message: error.message
+        });
+    }
+
+};
+
+const loginUser = async (req, res) => {
+    console.log("LOGIN API HIT");
+
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.json({
+                success: false,
+                message: "User doesn't exist"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.json({
+                success: false,
+                message: "Invalid credentials"
+            });
+        }
+
+        const token = createToken(user._id);
+
+        return res.json({
+            success: true,
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+
+        return res.json({
+            success: false,
+            message: error.message
+        });
+    }
+
+}
+
+export { loginUser, registerUser };
--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,35 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoute.js";
+import "./config/cloudinary.js";
+import productRouter from "./routes/productRoute.js";
 
 dotenv.config();
 
 const app = express();
-connectDB();
 
-const PORT = process.env.PORT || 4000;
+
+connectDB();
+//connectCloudinary();
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+
+
+
+
+
 // Route
 app.get("/", (req, res) => {
     res.send("API Working");
 });
+
+const PORT = process.env.PORT || 4000;
 
 // Start server
 app.listen(PORT, () => {
