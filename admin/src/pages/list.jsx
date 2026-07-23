3ac305b01dbb5ab64c09ee09c095b50a7fb42312
KANIKA DROUNA <duskybook02@gmail.com>
2026-07-23 22:45:07 +0530

    completed the file add.jsx for adding products in the admin dashboard

--- admin/src/pages/list.jsx
+++ admin/src/pages/list.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { backendUrl } from '../App'
 
 const list = () => {
+
+  const [list,setList] = useState([])
+
+  const fetchList = async () => {
+    try{
+      const response = await axios.get(backendUrl + '/api/product/list')
+      if(response.data.success){
+        setList(response.data.products);
+      }else{
+        
+      }
+    }catch (error){
+
+    }
+  }
+
+  useEffect(()=>{
+    fetchList()
+  },[])
+
   return (
-    <div>list</div>
+    <div>
+
+    </div>
   )
 }
 
--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -1,19 +1,26 @@
+import jwt from "jsonwebtoken";
+
 const adminAuth = async (req, res, next) => {
     try {
+        const { token } = req.headers;
 
-        const { email, password } = req.headers;
-
-        if (
-            email === process.env.ADMIN_EMAIL &&
-            password === process.env.ADMIN_PASSWORD
-        ) {
-            next();
-        } else {
-            res.json({
+        if (!token) {
+            return res.json({
                 success: false,
-                message: "Not Authorized"
+                message: "Not Authorized Login Again"
             });
         }
+
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+            return res.json({
+                success: false,
+                message: "Not Authorized Login Again"
+            });
+        }
+
+        next();
 
     } catch (error) {
         console.log(error);
