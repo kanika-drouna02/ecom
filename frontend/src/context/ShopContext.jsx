e585a48c88b416b062bd1261f01a05deec1f4f5d
KANIKA DROUNA <duskybook02@gmail.com>
2026-07-25 17:14:27 +0530

    user authentication feature for the frontend

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,8 +1,8 @@
 import { createContext, useEffect } from "react";
-import { products } from "../assets/assets";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios'
 
 
 export const ShopContext = createContext();
@@ -12,9 +12,15 @@ const ShopContextProvider = (props) => {
 
     const currency = '$';
     const delivery_fee = 10;
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
     const [search,setSearch] = useState('');
     const [showSearch,setShowSearch] =useState(false);
     const [cartItems,setCartItems] = useState({});
+    const [products, setProducts] = useState([]);
+   // const [token,setToken] = useState('')
+   const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
     const navigate = useNavigate();
 
     const addToCart = async (itemId,size) => {
@@ -88,6 +94,32 @@ const ShopContextProvider = (props) => {
         return totalAmount;
     }
 
+    const getProductsData = async ()=>{
+        try {
+            
+            const response = await axios.get(backendUrl + '/api/product/list')
+            
+            if (response.data.success) {
+                setProducts(response.data.products)
+            } else {
+                toast.error(response.data.message)
+            }          
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
+    useEffect(()=>{
+        getProductsData()
+    },[])
+
+    useEffect(()=>{
+        if(!token && localStorage.getItem('token')){
+            setToken(localStorage.getItem('token'))
+        }
+    },[])
 
     const value = {
         products,currency,delivery_fee,
@@ -95,7 +127,8 @@ const ShopContextProvider = (props) => {
         cartItems,addToCart,
         getCartCount, updateQuantity,
         getCartAmount,
-        navigate
+        navigate , backendUrl,
+        setToken, token
     }
     return (
         <ShopContext.Provider value={value}>
